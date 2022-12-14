af1200b0c2eead54fe700629be428a8ad1fa3378
smedziko <smedziko@htl-steyr.ac.at>
2022-12-14 10:23:16 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example._2223_4ahitn_footballmanager_phager_emiklaut_smedziko {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example._2223_4ahitn_footballmanager_phager_emiklaut_smedziko to javafx.fxml;
+    exports com.example._2223_4ahitn_footballmanager_phager_emiklaut_smedziko;
+}
