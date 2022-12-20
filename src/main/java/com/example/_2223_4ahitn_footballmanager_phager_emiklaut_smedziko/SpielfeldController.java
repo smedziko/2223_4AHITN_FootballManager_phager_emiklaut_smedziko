23a4e6783a7cf6137c133d47ff1a5e06ac4536e1
philipp04 <phager@htl-steyr.ac.at>
2022-12-20 16:44:39 +0100

    Spielfeld design

--- src/main/java/com/example/_2223_4ahitn_footballmanager_phager_emiklaut_smedziko/SpielfeldController.java
+++ src/main/java/com/example/_2223_4ahitn_footballmanager_phager_emiklaut_smedziko/SpielfeldController.java
@@ -1,5 +1,6 @@
 package com.example._2223_4ahitn_footballmanager_phager_emiklaut_smedziko;
 
+import com.example._2223_4ahitn_footballmanager_phager_emiklaut_smedziko.Model.Spieler;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -30,14 +31,26 @@ public class SpielfeldController {
         field_image.setImage(img);
         field_image.setPreserveRatio(false);
 
-        Circle r = new Circle();
-        r.setFill(new ImagePattern(spieler));
-        r.setRadius(18);
-        r.setLayoutX(50);
-        r.setLayoutY(298);
+        Spieler s = new Spieler(playable_field, "LV", false);
+        Spieler s2 = new Spieler(playable_field, "LV", true);
 
-        playable_field.getChildren().add(r);
+        Spieler s10 = new Spieler(playable_field, "RV", false);
+        Spieler s20 = new Spieler(playable_field, "RV", true);
 
+        Spieler s4 = new Spieler(playable_field, "LM", false);
+        Spieler s5 = new Spieler(playable_field, "LM", true);
+
+        Spieler sd = new Spieler(playable_field, "ZM", false);
+        Spieler sd2 = new Spieler(playable_field, "ZM", true);
+
+        Spieler sd234 = new Spieler(playable_field, "RM", false);
+        Spieler sd22 = new Spieler(playable_field, "RM", true);
+
+        Spieler torwart = new Spieler(playable_field, "TW", false);
+        Spieler torwart2 = new Spieler(playable_field, "TW", true);
+
+        Spieler torwart4 = new Spieler(playable_field, "ST", false);
+        Spieler torwart24 = new Spieler(playable_field, "ST", true);
 
 
 
